b983f21e8cdef15dbec6bfc6a8448ecce3db1d9d
Abhishek jindal <zeus1997aj@users.noreply.github.com>
2021-04-28 00:47:56 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Checkout from "./Checkout";
+import Checkout from "./checkout";
 function App() {
   const [sidebar, setSidebar] = useState(false);
   const showsidebar = () => setSidebar(!sidebar);
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <Switch>
-          <Route path='/checkout'>
+          <Route path="/checkout">
             <Checkout />
           </Route>
-          <Route path='/login'>
+          <Route path="/login">
             <h1>Login Page</h1>
           </Route>
-          <Route path='/'>
+          <Route path="/">
             <Header sidebar={sidebar} showsidebar={showsidebar} />
             <Home />
           </Route>
